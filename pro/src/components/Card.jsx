5eca7218b7f38f9ce762c515c43497218a058ce6
abniNew <abnew0923@gmail.com>
2023-09-08 12:27:35 -0700

    updated code about the Hook

--- pro/src/components/Card.jsx
+++ pro/src/components/Card.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import Title from "./Title";
 import Date from "./Date";
 import Author from "./Author";
@@ -6,12 +6,41 @@ import Summary from "./Summery";
 
 function Card({ title, author, date, summary }) {
 
+    const [comments, setComments] = useState([]); // Track comments using useState
+    const [likes, setLikes] = useState(0); // Track likes using useState
+
+    const addComment = () => {
+        const newComment = prompt('Add a comment:');
+        if (newComment) {
+          setComments([ ...comments, newComment]);
+        }
+      };
+    
+      const incrementLikes = () => {
+        setLikes(likes + 1);
+      };
+
     return (
         <div className="card">
             <Title title={title} />
             <Author author={author} />
             <Date date={date} />
             <Summary summary={summary} />
+
+            <button onClick={addComment}>Add Comment</button>
+            <button onClick={incrementLikes}>Like</button>
+                {/* Display number of likes and button to increment likes */}
+            <p>Likes: {likes}</p>
+            {comments.length > 0 && (
+        <div>
+          <h3>Comments:</h3>
+          <ul>
+            {comments.map((comment, index) => (
+              <li key={index}>{comment}</li>
+            ))}
+          </ul>
+        </div>
+      )}
         </div>
     )
 
